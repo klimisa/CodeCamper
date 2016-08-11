4c759694332c2969457c8bb789e22ea288e6426b
Antonios Klimis <klimis@intralot.com>
2016-08-11 16:03:39 +0300

    Initial commit;

---
+++ CodeCamper.Data/CodeCamperDbContext.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using CodeCamper.Model;
+
+namespace CodeCamper.Data
+{
+    public class CodeCamperDbContext : DbContext
+    {
+        public CodeCamperDbContext() 
+            :base(nameOrConnectionString: "CodeCamper") { }
+
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Attendance> Attendance { get; set; }
+
+        //Lookup Lists
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<TimeSlot> TimeSlots { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+
+            modelBuilder.Configurations.Add(new SessionConfiguration());
+            modelBuilder.Configurations.Add(new AttendanceConfiguration());
+        }
+    }
+
+    public class SessionConfiguration: EntityTypeConfiguration<Session>
+    {
+        public SessionConfiguration()
+        {
+            HasRequired(s => s.Speaker)
+                .WithMany(p => p.SpeakerSessions)
+                .HasForeignKey(s => s.SpeakerId);
+        }
+    }
+
+    public class AttendanceConfiguration: EntityTypeConfiguration<Attendance>
+    {
+        public AttendanceConfiguration()
+        {
+            HasKey(a => new {a.SessionId, a.PersonId});
+
+            HasRequired(a => a.Session)
+                .WithMany(s => s.AttendanceList)
+                .HasForeignKey(a => a.SessionId)
+                .WillCascadeOnDelete(false);
+
+            HasRequired(a => a.Person)
+                .WithMany(p => p.AttendanceList)
+                .HasForeignKey(a => a.PersonId)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
